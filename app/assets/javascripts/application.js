94cb4ffb3ee59e5cb5d9ce619387c99e3477e954
subhasish.dutta <subhasish.dutta@aurea.com>
2016-09-10 10:57:34 +0530

    Applied theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,8 +10,9 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require jquery
-//= require jquery_ujs
-//= require turbolinks
-//= require bootstrap-sprockets
-//= require_tree .
+//= require jquery.min
+//= require bootstrap.min
+//= require metisMenu.min
+//= require custom
+
+
